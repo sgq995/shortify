dc312b89b347a85e945934a1e01370d80bdb45a9
Sebastian G. Q <sgq995@gmail.com>
2023-05-17 16:01:27 +0000

    feat: initial http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello).service(echo))
+        .bind(("127.0.0.1", 5000))?
+        .run()
+        .await
 }
