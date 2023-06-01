dc1edef33e0ca5616cf152d5c092b17b6f6f01f1
Sebastian G. Q <sgq995@gmail.com>
2023-06-01 22:18:16 +0000

    feat: form data

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder, web};
+use serde::Deserialize;
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[derive(Deserialize)]
+struct UrlFormData {
+    url: String,
+}
+
 #[post("/")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+async fn create(form: web::Form<UrlFormData>) -> impl Responder {
+    form.url.to_owned()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello).service(echo))
+    HttpServer::new(|| App::new().service(hello).service(create))
         .bind(("127.0.0.1", 5000))?
         .run()
         .await
