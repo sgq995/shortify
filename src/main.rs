c7d68aa2a337adbdffeb58ee772ecaa79d35aa64
Sebastian G. Q <sgq995@gmail.com>
2023-06-14 21:35:09 -0500

    feat: logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware, web, App, HttpServer};
+use env_logger::Env;
 
 pub mod actions;
 pub mod database;
@@ -8,16 +9,19 @@ pub mod schema;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     let pool = database::connect();
 
     HttpServer::new(move || {
         App::new()
+            .wrap(middleware::Logger::default())
             .app_data(web::Data::new(pool.clone()))
             .service(routes::healthcheck)
             .service(routes::read)
             .service(routes::create)
     })
-    .bind(("127.0.0.1", 5000))?
+    .bind(("0.0.0.0", 5000))?
     .run()
     .await
 }
